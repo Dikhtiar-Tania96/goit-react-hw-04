7f92fba36870f1e234c43cd95a7c9ac407546413
Dikhtiar Tania <you@example.com>
2024-06-07 05:52:40 +0300

    push new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from 'react'
 import './App.css'
-import Loader from './components/Loader/Loader'
+// import Loader from './components/Loader/Loader'
 
 function App() {
+  useEffect(()=>{
+    const searchI = async () => {
+      const data = await searchImage()
+      console.log('data', data)
+    }
+    searchI()
+  })
+
   return (
     <>
-    <Loader/>
     </>
   )
 }
