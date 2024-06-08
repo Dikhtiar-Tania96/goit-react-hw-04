bc06bc07d4d99ce0805154664f4336153c3e4f15
Dikhtiar Tania <you@example.com>
2024-06-08 13:30:12 +0300

    add new file error and try/catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
 import { useEffect, useState } from "react";
 import { searchImagesApi } from "./api";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
 
 function App() {
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const searchImages = async () => {
+      try {
+      setIsLoading(true)
       const data = await searchImagesApi();
-      console.log("data", data);
-      setImages(data);
+      setImages(data)
+      setIsLoading(false)
+      } catch (error) {
+        setError(true)
+      }finally {
+        setIsLoading(false)
+      }
+      
     };
     searchImages();
   }, []);
 
-// const handleImageClick=(image)=>{
-// }
 
-  return <> 
-  <ImageGallery images={images} 
+  return (<>
+  {isLoading && <Loader/>}
+  {error && <p>Oops....ERROR</p>}
+   {images.length>0 && <ImageGallery images={images}/>} 
+  {/* <ImageGallery images={images} 
   // onImageClick={handleImageClick} 
-  />
+  /> */}
 
-  </>;
+  </>)
 }
 
 export default App;
